88a9d0547dbad292c7663cf0d7d108dce09b6eb6
Resul Ozdemir <zemirsd9@gmail.com>
2022-10-16 22:33:01 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace XOX_oyunu
+{
+    partial class XOX
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(XOX));
+            this.topLeft = new System.Windows.Forms.Button();
+            this.topMiddle = new System.Windows.Forms.Button();
+            this.topRight = new System.Windows.Forms.Button();
+            this.leftMiddle = new System.Windows.Forms.Button();
+            this.middle = new System.Windows.Forms.Button();
+            this.middleRight = new System.Windows.Forms.Button();
+            this.buttomLeft = new System.Windows.Forms.Button();
+            this.buttomMiddle = new System.Windows.Forms.Button();
+            this.buttomRight = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.yenidenOyna = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // topLeft
+            // 
+            resources.ApplyResources(this.topLeft, "topLeft");
+            this.topLeft.Name = "topLeft";
+            this.topLeft.UseVisualStyleBackColor = true;
+            this.topLeft.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // topMiddle
+            // 
+            resources.ApplyResources(this.topMiddle, "topMiddle");
+            this.topMiddle.Name = "topMiddle";
+            this.topMiddle.UseVisualStyleBackColor = true;
+            this.topMiddle.Click += new System.EventHandler(this.topMiddle_Click);
+            // 
+            // topRight
+            // 
+            resources.ApplyResources(this.topRight, "topRight");
+            this.topRight.Name = "topRight";
+            this.topRight.UseVisualStyleBackColor = true;
+            this.topRight.Click += new System.EventHandler(this.topRight_Click);
+            // 
+            // leftMiddle
+            // 
+            resources.ApplyResources(this.leftMiddle, "leftMiddle");
+            this.leftMiddle.Name = "leftMiddle";
+            this.leftMiddle.UseVisualStyleBackColor = true;
+            this.leftMiddle.Click += new System.EventHandler(this.leftMiddle_Click);
+            // 
+            // middle
+            // 
+            resources.ApplyResources(this.middle, "middle");
+            this.middle.Name = "middle";
+            this.middle.UseVisualStyleBackColor = true;
+            this.middle.Click += new System.EventHandler(this.middle_Click);
+            // 
+            // middleRight
+            // 
+            resources.ApplyResources(this.middleRight, "middleRight");
+            this.middleRight.Name = "middleRight";
+            this.middleRight.UseVisualStyleBackColor = true;
+            this.middleRight.Click += new System.EventHandler(this.middleRight_Click);
+            // 
+            // buttomLeft
+            // 
+            resources.ApplyResources(this.buttomLeft, "buttomLeft");
+            this.buttomLeft.Name = "buttomLeft";
+            this.buttomLeft.UseVisualStyleBackColor = true;
+            this.buttomLeft.Click += new System.EventHandler(this.buttomLeft_Click);
+            // 
+            // buttomMiddle
+            // 
+            this.buttomMiddle.BackColor = System.Drawing.Color.Gainsboro;
+            resources.ApplyResources(this.buttomMiddle, "buttomMiddle");
+            this.buttomMiddle.Name = "buttomMiddle";
+            this.buttomMiddle.UseVisualStyleBackColor = false;
+            this.buttomMiddle.Click += new System.EventHandler(this.buttomMiddle_Click);
+            // 
+            // buttomRight
+            // 
+            this.buttomRight.Cursor = System.Windows.Forms.Cursors.Default;
+            resources.ApplyResources(this.buttomRight, "buttomRight");
+            this.buttomRight.Name = "buttomRight";
+            this.buttomRight.UseVisualStyleBackColor = true;
+            this.buttomRight.Click += new System.EventHandler(this.buttomRight_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Name = "label1";
+            // 
+            // yenidenOyna
+            // 
+            resources.ApplyResources(this.yenidenOyna, "yenidenOyna");
+            this.yenidenOyna.Name = "yenidenOyna";
+            this.yenidenOyna.UseVisualStyleBackColor = true;
+            this.yenidenOyna.Click += new System.EventHandler(this.yenidenOyna_Click);
+            // 
+            // XOX
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateGray;
+            this.Controls.Add(this.yenidenOyna);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttomRight);
+            this.Controls.Add(this.buttomMiddle);
+            this.Controls.Add(this.buttomLeft);
+            this.Controls.Add(this.middleRight);
+            this.Controls.Add(this.middle);
+            this.Controls.Add(this.leftMiddle);
+            this.Controls.Add(this.topRight);
+            this.Controls.Add(this.topMiddle);
+            this.Controls.Add(this.topLeft);
+            this.MaximizeBox = false;
+            this.Name = "XOX";
+            this.Load += new System.EventHandler(this.XOX_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button topLeft;
+        private System.Windows.Forms.Button topMiddle;
+        private System.Windows.Forms.Button topRight;
+        private System.Windows.Forms.Button leftMiddle;
+        private System.Windows.Forms.Button middle;
+        private System.Windows.Forms.Button middleRight;
+        private System.Windows.Forms.Button buttomLeft;
+        private System.Windows.Forms.Button buttomMiddle;
+        private System.Windows.Forms.Button buttomRight;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button yenidenOyna;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+//githuba at
+//programi ingilizce değişkenlere cevir
+
+
+namespace XOX_oyunu
+{
+    public partial class XOX : Form
+    {
+        int turn = 0,win = 0,flag = 0;
+        //turn = 0 X-turn
+        //turn = 1 O-turn
+
+        Image X = new Bitmap(@"C:\Users\erdem\Desktop\görsel programlama\1.hafta\x.png");
+        Image O = new Bitmap(@"C:\Users\erdem\Desktop\görsel programlama\1.hafta\o.png");
+
+        void xTurn()
+        {
+            turn++;
+            winer(flag);
+        }
+
+        void oTurn()
+        {
+            turn--;
+            winer(flag);
+        }
+
+        void xWin()
+        {
+            label1.Text = "X WIN";
+            label1.Visible = Enabled;   
+            win += 1;
+            yenidenOyna.Visible = Enabled;
+        }
+
+        void oWin()
+        {
+            label1.Text = "O WIN";
+            label1.Visible = Enabled;
+            win += 1;
+            yenidenOyna.Visible = Enabled;
+        }
+
+        void winer(int flag)
+        {
+            
+
+            if (topLeft.BackgroundImage == X && topMiddle.BackgroundImage == X && topRight.BackgroundImage == X)
+            {
+                xWin();
+            }
+
+            else if (leftMiddle.BackgroundImage == X && middle.BackgroundImage == X && middleRight.BackgroundImage == X)
+            {
+                xWin();
+            }
+
+            else if (buttomLeft.BackgroundImage == X && buttomMiddle.BackgroundImage == X && buttomRight.BackgroundImage == X)
+            {
+                xWin();
+            }
+
+            else if (topLeft.BackgroundImage == X && leftMiddle.BackgroundImage == X && buttomLeft.BackgroundImage == X)
+            {
+                xWin();
+            }
+
+            else if (topMiddle.BackgroundImage == X && middle.BackgroundImage == X && buttomMiddle.BackgroundImage == X)
+            {
+                xWin();
+            }
+
+            else if (topRight.BackgroundImage == X && middleRight.BackgroundImage == X && buttomRight.BackgroundImage ==X)
+            {
+                xWin();
+            }
+
+            else if (topLeft.BackgroundImage == X && middle.BackgroundImage == X && buttomRight.BackgroundImage == X)
+            {
+                xWin();
+            }
+
+            else if (topRight.BackgroundImage == X && middle.BackgroundImage == X && buttomLeft.BackgroundImage == X)
+            {
+                xWin();
+            }
+
+            else if (topLeft.BackgroundImage == O && topMiddle.BackgroundImage == O && topRight.BackgroundImage == O)
+            {
+                oWin();
+            }
+
+            else if (leftMiddle.BackgroundImage == O && middle.BackgroundImage == O && middleRight.BackgroundImage == O)
+            {
+                oWin();
+            }
+
+            else if (buttomLeft.BackgroundImage == O && buttomMiddle.BackgroundImage == O && buttomRight.BackgroundImage == O)
+            {
+                oWin();
+            }
+
+            else if (topLeft.BackgroundImage == O && leftMiddle.BackgroundImage == O && buttomLeft.BackgroundImage == O)
+            {
+                oWin();
+            }
+
+            else if (topMiddle.BackgroundImage == O && middle.BackgroundImage == O && buttomMiddle.BackgroundImage == O)
+            {
+                oWin();
+            }
+
+            else if (topRight.BackgroundImage == O && middleRight.BackgroundImage == O && buttomRight.BackgroundImage == O)
+            {
+                oWin();
+            }
+
+            else if (topLeft.BackgroundImage == O && middle.BackgroundImage == O && buttomRight.BackgroundImage == O)
+            {
+                oWin();
+            }
+
+            else if (topRight.BackgroundImage == O && middle.BackgroundImage == O && buttomLeft.BackgroundImage == O)
+            {
+                oWin();
+            }
+
+        }
+
+        public XOX()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (turn == 0 && win!=1)
+            {
+                topLeft.BackgroundImage = X;
+                topLeft.Enabled = false;
+                xTurn();
+
+            }
+            else if(turn == 1 && win != 1)
+            {
+                topLeft.BackgroundImage = O;
+                topLeft.Enabled = false;
+                oTurn();
+            }
+        }
+
+        private void buttomRight_Click(object sender, EventArgs e)
+        {
+            if (turn == 0 && win != 1)
+            {
+                buttomRight.BackgroundImage = X;
+                buttomRight.Enabled = false;
+                xTurn();
+            }
+            else if (turn == 1 && win != 1)
+            {
+                buttomRight.BackgroundImage = O;
+                buttomRight.Enabled = false;
+                oTurn();
+            }
+        }
+
+        private void topMiddle_Click(object sender, EventArgs e)
+        {
+            if (turn == 0 && win != 1)
+            {
+                topMiddle.BackgroundImage = X;
+                topMiddle.Enabled = false;
+                xTurn();
+            }
+            else if (turn == 1 && win != 1)
+            {
+                topMiddle.BackgroundImage = O;
+                topMiddle.Enabled = false;
+                oTurn();
+            }
+        }
+
+        private void topRight_Click(object sender, EventArgs e)
+        {
+            if (turn == 0 && win != 1)
+            {
+                topRight.BackgroundImage = X;
+                topRight.Enabled = false;
+                xTurn();
+            }
+            else if (turn == 1 && win != 1)
+            {
+                topRight.BackgroundImage = O;
+                topRight.Enabled = false;
+                oTurn();
+            }
+        }
+
+        private void leftMiddle_Click(object sender, EventArgs e)
+        {
+            if (turn == 0 && win != 1)
+            {
+                leftMiddle.BackgroundImage = X;
+                leftMiddle.Enabled = false;
+                xTurn();
+            }
+            else if (turn == 1 && win != 1)
+            {
+                leftMiddle.BackgroundImage = O;
+                leftMiddle.Enabled = false;
+                oTurn();
+            }
+        }
+
+        private void middle_Click(object sender, EventArgs e)
+        {
+            if (turn == 0 && win != 1)
+            {
+                middle.BackgroundImage = X;
+                middle.Enabled = false;
+                xTurn();
+            }
+            else if (turn == 1 && win != 1)
+            {
+                middle.BackgroundImage = O;
+                middle.Enabled = false;
+                oTurn();
+            }
+        }
+
+        private void middleRight_Click(object sender, EventArgs e)
+        {
+            if (turn == 0 && win != 1)
+            {
+                middleRight.BackgroundImage = X;
+                middleRight.Enabled = false;
+                xTurn();
+            }
+            else if (turn == 1 && win != 1)
+            {
+                middleRight.BackgroundImage = O;
+                middleRight.Enabled = false;
+                oTurn();
+            }
+        }
+
+        private void buttomMiddle_Click(object sender, EventArgs e)
+        {
+            if (turn == 0 && win != 1)
+            {
+                buttomMiddle.BackgroundImage = X;
+                buttomMiddle.Enabled = false;
+                xTurn();
+            }
+            else if (turn == 1 && win != 1)
+            {
+                buttomMiddle.BackgroundImage = O;    
+                buttomMiddle.Enabled = false;
+                oTurn();
+            }
+
+        }
+
+        private void buttomLeft_Click(object sender, EventArgs e)
+        {
+            if (turn == 0 && win != 1)
+            {
+                buttomLeft.BackgroundImage = X;
+                buttomLeft.Enabled = false;
+                xTurn();
+            }
+            else if (turn == 1 && win != 1)
+            {
+                buttomLeft.BackgroundImage = O;
+                buttomLeft.Enabled = false;
+                oTurn();
+            }
+        }
+
+        private void yenidenOyna_Click(object sender, EventArgs e)
+        {
+            label1.Visible = false;
+            turn = 0;
+            win = 0;
+            yenidenOyna.Visible = false;
+            
+            buttomRight.BackgroundImage = default;
+            topRight.BackgroundImage = default;
+            middleRight.BackgroundImage = default;
+            buttomMiddle.BackgroundImage = default;
+            middle.BackgroundImage = default;
+            topMiddle.BackgroundImage = default;
+            topLeft.BackgroundImage = default;
+            leftMiddle.BackgroundImage = default;
+            buttomLeft.BackgroundImage = default;
+            
+            buttomRight.Enabled = true;
+            topRight.Enabled = true;
+            middleRight.Enabled = true;
+            buttomMiddle.Enabled = true;
+            middle.Enabled = true;
+            topMiddle.Enabled = true;
+            topLeft.Enabled = true;
+            leftMiddle.Enabled = true;
+            buttomLeft.Enabled = true;
+        }
+
+        private void XOX_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
